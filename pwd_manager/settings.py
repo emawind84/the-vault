0c0514e0d57299e7829bd36ad6b6d6f70591630c
Emanuele Disco <emawind84@gmail.com>
2018-10-11 18:03:10 +0900

    update

--- pwd_manager/settings.py
+++ pwd_manager/settings.py
@@ -110,15 +110,15 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTHENTICATION_BACKENDS = (
-    'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
-    'social_core.backends.google.GoogleOpenId',  # for Google authentication
-    'social_core.backends.google.GoogleOAuth2',  # for Google authentication
-    'social_core.backends.github.GithubOAuth2',  # for Github authentication
+    #'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+    #'social_core.backends.google.GoogleOpenId',  # for Google authentication
+    #'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+    #'social_core.backends.github.GithubOAuth2',  # for Github authentication
     #'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
     #'auth.settings_backend.SettingsBackend',
-    #'auth.ldap_backend.LDAPBackend1',
+    'auth.ldap_backend.LDAPBackend1',
     #'auth.ldap_backend.LDAPBackend2',
-    #'auth.pmis_backend.PMISBackend',
+    'auth.pmis_backend.PMISBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
 
